ce91ec7ea10c5865dda780bc6f516bc4b604ebb8
nini0m <90498702+nini0m@users.noreply.github.com>
2021-10-24 16:39:58 +0400

    add website

---
+++ css/script.js
@@ -0,0 +1,16 @@
+document.getElementById('send-mail').addEventListener('click', function(){
+    alert('Your E-mail address is sent.')
+})
+document.getElementById('send').addEventListener('click', function(){
+    alert('Your information is sent.')
+})
+document.getElementById('send-email').addEventListener('click', function(){
+    alert('Your E-mail address is sent.')
+})
+
+let Navigation=document.getElementById('Navigation');
+let burgerIcon=document.getElementById('burgerIcon');
+
+burgerIcon.addEventListener('click', function() {
+  Navigation.classList.toggle('active');
+});
